cc9e71da047efd64dc003a78a199e0f334a1d3ec
Keisha <keisha@Keishas-Mac-mini.local>
2026-03-07 09:37:05 +0100

    Changed the webpack config

--- wepback.config.js
+++ wepback.config.js
@@ -12,7 +12,7 @@ export default {
     entry: "./src/index.js",
     output: {
         filename: "main.js",
-        path: path.resolve(__dirname, "dist/js"),
+        path: path.resolve(__dirname, "dist/"),
         publicPath: './',
         clean: true,
     },
